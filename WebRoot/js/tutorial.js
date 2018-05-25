5a3ef839d42893e018284343669c2d46cdcbf0e6
DevL0rd <dmhzmxn@gmail.com>
2018-05-25 21:14:23 +0000

    preparing to add turtorial

---
+++ WebRoot/js/tutorial.js
@@ -0,0 +1,6 @@
+// var userDrop = document.getElementById("userDrop")
+// var popper = new Popper(userDrop, function () {
+//     console.log("something")
+// }, {
+//         placement: 'bottom'
+//     });
